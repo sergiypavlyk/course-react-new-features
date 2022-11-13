9825bb32ab201448ae830fd7a3b18baf6d85c579
sergiypavlyk <88386182+sergiypavlyk@users.noreply.github.com>
2022-11-13 18:46:39 +0200

    add custom hook

--- src/components/AddNoteForm.js
+++ src/components/AddNoteForm.js
@@ -1,10 +1,12 @@
-import React, { useContext, useState } from 'react';
-import NotesContext from '../context/notes-context';
+import React, { useState, useContext } from 'react'
+import NotesContext from '../context/notes-context'
+import useMousePosition from '../hooks/useMousePosition'
 
 const AddNoteForm = () => {
-  const [title, setTitle] = useState('');
-  const [body, setBody] = useState('');
-  const { dispatch } = useContext(NotesContext);
+  const { dispatch } = useContext(NotesContext)
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+  const position = useMousePosition()
 
   const addNote = (e) => {
     e.preventDefault()
@@ -18,14 +20,14 @@ const AddNoteForm = () => {
   }
 
   return (
-    <div>
-      <p>Add note</p>
+    <>
+      <p>Add note {position.x} - {position.y}</p>
       <form onSubmit={addNote}>
         <input value={title} onChange={(e) => setTitle(e.target.value)} />
         <textarea value={body} onChange={(e) => setBody(e.target.value)}></textarea>
         <button>add note</button>
       </form>
-    </div>
+    </>
   )
 }
 
