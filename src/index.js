3c11ce676dd3a35e6c77526c2879e9e989541426
sergiypavlyk <88386182+sergiypavlyk@users.noreply.github.com>
2022-11-13 18:14:11 +0200

    note app use state & use effect hooks

--- src/index.js
+++ src/index.js
@@ -1,24 +1,66 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
-const App = () => {
-  const [count, setCount] = useState(10);
+const NoteApp = () => {
+  const [notes, setNotes] = useState([])
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
 
-  const increment = () => MediaStreamAudioDestinationNode(count + 1);
-  const decrement = () => MediaStreamAudioDestinationNode(count - 1)
+  const addNote = (e) => {
+    e.preventDefault()
+    setNotes([
+      ...notes,
+      { title, body }
+    ])
+    setTitle('')
+    setBody('')
+  }
+
+  const removeNote = (title) => setNotes(notes.filter((note) => note.title !== title));
+
+  useEffect(() => {
+    const notesData = JSON.parse(localStorage.getItem('notes'))
+    if (notesData) setNotes(notesData);
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('notes', JSON.stringify(notes))
+  }, [notes])
 
   return (
     <div>
-      <p>The current count is {count}</p>
-      <button onClick={increment}>+1</button>
-      <button onClick={decrement}>-1</button>
+      <h1>Notes</h1>
+      {notes.map((note) => (
+        <Note key={note.title} note={note} removeNote={removeNote}/>
+      ))}
+      <p>Add note</p>
+      <form onSubmit={addNote}>
+        <input value={title} onChange={(e) => setTitle(e.target.value)} />
+        <textarea value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+        <button>add note</button>
+      </form>
     </div>
-  );
-};
+  )
+}
+
+const Note = ({ note, removeNote }) => {
+  useEffect(() => {
+    console.log('Setting up effect!')
+    return () => console.log('Cleaning up effect!');
+  }, [])
+
+  return (
+    <div>
+      <h3>{note.title}</h3>
+      <p>{note.body}</p>
+      <button onClick={() => removeNote(note.title)}>x</button>
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App/>);
+root.render(<NoteApp/>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
