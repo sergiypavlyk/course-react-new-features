5a37b8f665d61073c63b762bb32ce490552682ce
sergiypavlyk <88386182+sergiypavlyk@users.noreply.github.com>
2022-11-13 17:54:00 +0200

    First commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const App = () => {
+  const [count, setCount] = useState(10);
+
+  const increment = () => MediaStreamAudioDestinationNode(count + 1);
+  const decrement = () => MediaStreamAudioDestinationNode(count - 1)
+
+  return (
+    <div>
+      <p>The current count is {count}</p>
+      <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App/>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
